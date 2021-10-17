34a2caa0c6ad9f456e97eefdedc16215df923e97
rozan123 <66568997+rozan123@users.noreply.github.com>
2021-10-17 15:21:37 -0500

    first commit

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,103 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG ="LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btLogin;
+    private Button btRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//        if(ParseUser.getCurrentUser() != null){
+//            goMainActivity();
+//        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btLogin = findViewById(R.id.btnLogin);
+        btRegister = findViewById(R.id.btnRegister);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onclick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(username, password);
+                
+            }
+        });
+    }
+//for signUp
+    private void signupUser(String username, String password) {
+        Log.i(TAG,"Attempting to login user "+ username);
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with signup", e);
+                    Toast.makeText(LoginActivity.this,"issue with signup",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //TODO: navigate to the main activity if the user has signed in properly
+                goMainActivity();
+                Toast.makeText(LoginActivity.this,"Success!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loginUser(String username, String password){
+        Log.i(TAG,"Attempting to login user "+ username);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if( e !=null){
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this,"issue with login",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //TODO: navigate to the main activity if the user has signed in properly
+                goMainActivity();
+               Toast.makeText(LoginActivity.this,"Success!",Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+    }
+    private void goMainActivity(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+
+    }
+}
